1d9c8b3df2019b0bb2cadcb70a6a7e8043e32167
Ruslan Salikhov <ruslan.salihov@gmail.com>
2018-06-24 23:54:32 +0500

    Dev version

---
+++ styles.759902c8cc5d9bcd2608.js
@@ -0,0 +1,18 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[15],{
+
+/***/ 1402:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ 2556:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+}]);
+//# sourceMappingURL=styles.js.map
